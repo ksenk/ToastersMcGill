58a7339b365a44815d9ed1f5dfdc6d705be1c65f
Ksenia <ksenia.kolosova@mail.mcgill.ca>
2018-02-03 15:16:55 -0500

    withcoffee img

--- Inventory/Inventory/RootViewController.swift
+++ Inventory/Inventory/RootViewController.swift
@@ -12,7 +12,7 @@ class RootViewController: UIViewController, UIPageViewControllerDelegate {
 
     var pageViewController: UIPageViewController?
 
-
+New Thing
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
